3c6c7a5435c79a0f493d1c2750420ddb125f6d35
Enrico Giacomazzi <enrico@hyperware.io>
2020-04-22 12:51:48 +0200

    Add project files.

---
+++ DemoWeather/Models/WeatherModel.cs
@@ -0,0 +1,75 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace DemoWeather.Models
+{
+    public class WeatherModel
+    {
+        public City city { get; set; }
+        public string cod { get; set; }
+        public float message { get; set; }
+        public int cnt { get; set; }
+        public List[] list { get; set; }
+    }
+
+    public class City
+    {
+        public int id { get; set; }
+        public string name { get; set; }
+        public Coord coord { get; set; }
+        public string country { get; set; }
+        public int population { get; set; }
+        public int timezone { get; set; }
+    }
+
+    public class Coord
+    {
+        public float lon { get; set; }
+        public float lat { get; set; }
+    }
+
+    public class List
+    {
+        public int dt { get; set; }
+        public int sunrise { get; set; }
+        public int sunset { get; set; }
+        public Temp temp { get; set; }
+        public Feels_Like feels_like { get; set; }
+        public int pressure { get; set; }
+        public int humidity { get; set; }
+        public Weather[] weather { get; set; }
+        public float speed { get; set; }
+        public int deg { get; set; }
+        public int clouds { get; set; }
+        public float rain { get; set; }
+    }
+
+    public class Temp
+    {
+        public float day { get; set; }
+        public float min { get; set; }
+        public float max { get; set; }
+        public float night { get; set; }
+        public float eve { get; set; }
+        public float morn { get; set; }
+    }
+
+    public class Feels_Like
+    {
+        public float day { get; set; }
+        public float night { get; set; }
+        public float eve { get; set; }
+        public float morn { get; set; }
+    }
+
+    public class Weather
+    {
+        public int id { get; set; }
+        public string main { get; set; }
+        public string description { get; set; }
+        public string icon { get; set; }
+    }
+
+}
---
+++ DemoWeather/wwwroot/js/index.js
@@ -0,0 +1,30 @@
+﻿
+
+
+function loadMap(handler) {
+    const mymap = L.map('map').setView([47.3774497, 8.5016957], 4);
+
+    L.tileLayer('https://api.mapbox.com/styles/v1/{id}/tiles/{z}/{x}/{y}?access_token={accessToken}', {
+        attribution: 'Map data &copy; <a href="https://www.openstreetmap.org/">OpenStreetMap</a> contributors, <a href="https://creativecommons.org/licenses/by-sa/2.0/">CC-BY-SA</a>, Imagery © <a href="https://www.mapbox.com/">Mapbox</a>',
+        maxZoom: 18,
+        id: 'mapbox/streets-v11',
+        tileSize: 512,
+        zoomOffset: -1,
+        accessToken: 'pk.eyJ1Ijoic2lyamFjayIsImEiOiJjazliMG1yaXcwZjByM25ueXppNHplY2YyIn0.8Op4YDuYexp0y2NiDoESRQ'
+    }).addTo(mymap);
+
+    let marker;
+
+    mymap.on('click', ({ latlng }) => {
+        if (!marker) {
+            marker = L.marker([latlng.lat, latlng.lng]).addTo(mymap);
+        } else {
+            marker.setLatLng(latlng);
+        }
+
+        handler.invokeMethodAsync('GetWeatherDataAsync', latlng.lat, latlng.lng).then(x => {
+            console.log(x);
+        })
+    })
+}
+
